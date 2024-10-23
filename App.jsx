352a02d4008c9e9c819f4d62584b4f71a9e6e5a4
Monaaaasingh <113367709+Monaaaasingh@users.noreply.github.com>
2024-10-23 16:32:36 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,36 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from './components/Login';
+import Service from './components/Service';
+import Navbar from './components/Navbar';
+import Attendence from './Attendence/Attendence';
+import ViewAttendance from './Attendence/ViewAttendance'; 
+import Signup from './components/Signup';
+import Home from './Attendence/Home';
+import Admission from './components/Admission';
+import 'react-toastify/dist/ReactToastify.css';
+import { ToastContainer } from 'react-toastify';
+
+function App() {
+  return (
+    <div className='contain bt-0'>
+      <div className="cont h-screen w-full mt-0">
+        <BrowserRouter>
+          <Routes>
+            <Route path='/' element={<Navbar/>}>
+              <Route index element={<Home/>}/>
+              <Route path="login" element={<Login/>} />
+              <Route path="signup" element={<Signup/>} />
+              <Route path='attendence' element={<Attendence/>}/>
+              <Route path='admission' element={<Admission/>}/>
+              <Route path='service' element={<Service/>}/>
+              <Route path='viewattendance' element={<ViewAttendance/>}/> 
+            </Route>
+          </Routes>
+          <ToastContainer />
+        </BrowserRouter>
+      </div>
+    </div>
+  );
+}
+
+export default App;
